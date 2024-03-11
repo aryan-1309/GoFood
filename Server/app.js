e31180de16de94f0d8ba2728bc8b6e483cd36647
aryan1309 <aryanjangir1309@gmail.com>
2024-03-11 23:20:38 +0530

    Added Createuser feature in backend

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const app = express()
-const PORT = 5000
 const mongoDB = require("./db")
 mongoDB()
+
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send("Hello my food")
 })
 
+app.use('/api',require("./Routes/CreateUser"))
+
+const PORT = 5000
 app.listen(PORT,()=>{
     console.log(`Backend Server Is Running on PORT ${PORT}`)
 })
--- Server/db.js
+++ Server/db.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose")
-const URL = 'mongodb+srv://aryanjangir1309:aryan1309@cluster0.du6sopg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const URL = 'mongodb+srv://aryanjangir1309:aryan1309@cluster0.du6sopg.mongodb.net/myfood?retryWrites=true&w=majority&appName=Cluster0'
 
 const mongoDB = async () => {
-
     try {
-        await mongoose.connect(URL,{useNewUrlParser: true})
+        await mongoose.connect(URL, { useNewUrlParser: true })
         console.log("MongoDB is Connected Successfully")
-    } catch (error) {
-        console.log(error)
-    }
 
+        const fetched_data = await mongoose.connection.db.collection("food_items")
+        const data = await fetched_data.find({}).toArray()
+        // console.log(data)
+    } catch (error) {
+        console.error('Error while fetching data of food_items collection:', error.message)
+    }
 }
 
 module.exports = mongoDB
